d58b7ecae398d2e9e8556179c72cbf0d3db93dd7
miyamotomizuki <miiiisun@icloud.com>
2023-05-26 16:16:35 +0900

    エラーメッセージ重複を防ぐバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :family_name,  presence: true, allow_blank: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, allow_blank: true
+  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :family_name_kana, presence: true, allow_blank: true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, allow_blank: true
   validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{8,}\z/ }, allow_blank: true
+  validates :email, format: { with:  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, presence: true, allow_blank: true
 end
