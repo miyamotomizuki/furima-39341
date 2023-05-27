4a350c846fdc42836483ac01aad4e2e920ff16d2
miyamotomizuki <miiiisun@icloud.com>
2023-05-27 23:00:02 +0900

    productモデルにActiveHashを用いたアソシエーション定義

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,16 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :region
+  belongs_to :status
+  belongs_to :shipping_date
+  belongs_to :shipping_cost
+  belongs_to :category
+  
   belongs_to :user
   has_one_attached :image
 
 
-  validates :title, :explain, :price, presence: true
-  validates :region_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :name, :explain, :price, presence: true
+  validates :region_id, :status_id, :shipping_date_id, :shipping_cost_id, :category_id, numericality: { other_than: 1, message: "can't be blank"} 
 
 end
