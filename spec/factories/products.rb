79b2174bc5d0662082eda388f585c94855a1539d
miyamotomizuki <miiiisun@icloud.com>
2023-05-29 14:54:28 +0900

    ActiveStorageを用いたテストコード修正

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :product do
     name {Faker::Lorem.sentence}
-    image {Faker::Lorem.sentence}
     explain {Faker::Lorem.sentence}
     association :user 
     category_id           {2}
@@ -10,5 +9,9 @@ FactoryBot.define do
     shipping_date_id      {2}
     region_id             {2}
     price                 {5000}
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test-sample.png'), filename: 'test-sample.png')
+    end
   end
 end
