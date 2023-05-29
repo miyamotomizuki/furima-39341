c20b8a2675166ce0ec89eab8c4ec62966fa01f19
miyamotomizuki <miiiisun@icloud.com>
2023-05-29 15:22:43 +0900

    未選択時の表示「--」→「---」へ変更

--- app/models/shipping_date.rb
+++ app/models/shipping_date.rb
@@ -1,6 +1,6 @@
 class ShippingDate < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
+    { id: 1, name: '---' },
     { id: 2, name: '1~2日後' },
     { id: 3, name: '2~3日後' },
     { id: 4, name: '4~7日後' }
