473ca19d5e08e8ec5259d5ff06c2ac93713002cb
miyamotomizuki <miiiisun@icloud.com>
2023-05-30 17:33:24 +0900

    【修正】未ログイン時の削除許可設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   def index
     @products = Product.all.order('created_at DESC')
@@ -37,8 +37,10 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product.destroy
-    redirect_to action: :index
+    if current_user.id == @product.user_id
+      @product.destroy
+    end
+      redirect_to root_path
   end
 
   private
