5887d9771f6f3b6a38e6b08b26bbdcbb5e47e77b
miyamotomizuki <miiiisun@icloud.com>
2023-05-30 14:59:20 +0900

    ルーティングにupdateアクション定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
-  resources :products, only: [:index, :new, :create, :show, :edit]
+  resources :products, only: [:index, :new, :create, :show, :edit, :update]
 
 
 end
