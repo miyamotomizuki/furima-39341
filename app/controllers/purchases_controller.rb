059ea45a6f32d56ea33edd1320365ee2ced2950c
miyamotomizuki <miiiisun@icloud.com>
2023-06-01 21:00:07 +0900

    決済処理の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,6 +8,7 @@ class PurchasesController < ApplicationController
     @product = Product.find(params[:product_id])
     @purchase_buyers = PurchaseBuyer.new(purchase_params)
     if @purchase_buyers.valid?
+      pay_item
       @purchase_buyers.save
       redirect_to root_path
     else
@@ -20,5 +21,14 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_buyer).permit(:post_code, :city, :block, :building, :phone, :region_id).merge(user_id: current_user.id, product_id: @product.id,token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_e8939bbe0445ff53540ae19d"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @product.price,  # 商品の値段
+      card: params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
