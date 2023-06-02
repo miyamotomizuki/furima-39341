aa7e9d23e5078fe807e50d11ee1249a460b1bbed
miyamotomizuki <miiiisun@icloud.com>
2023-06-02 14:53:25 +0900

    【修正】models\purchase_buyer.rbリファタリング

--- app/models/purchase_buyer.rb
+++ app/models/purchase_buyer.rb
@@ -3,19 +3,15 @@ class PurchaseBuyer
   attr_accessor :post_code, :city, :block, :building, :phone, :region_id, :user_id, :product_id, :token
 
   with_options presence: true do
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :phone, format: { with: /\A[0-9]{10,11}\z/ }
+    validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :block
     validates :user_id
     validates :product_id
     validates :token
   end
-
-  validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-  validates :post_code, presence: true, allow_blank: true
-  validates :phone, format: { with: /\A[0-9]{10,11}\z/ }
-  validates :phone, presence: true, allow_blank: true
-
-  validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     purchase = Purchase.create(user_id: user_id, product_id: product_id)
