0adc56b5ff2642ec795cf03513d2995589d786db
miyamotomizuki <miiiisun@icloud.com>
2023-06-02 22:42:10 +0900

    日本語表記へ変更

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -24,72 +24,72 @@ RSpec.describe Product, type: :model do
       it 'userが紐付いていなければ出品できない' do
         @product.user = nil
         @product.valid?
-        expect(@product.errors.full_messages).to include('User must exist')
+        expect(@product.errors.full_messages).to include("Userを入力してください")
       end
       it '商品名がないと登録できない' do
         @product.name = ''
         @product.valid?
-        expect(@product.errors.full_messages).to include("Name can't be blank")
+        expect(@product.errors.full_messages).to include("商品名を入力してください")
       end
 
       it '商品の説明がないと登録できない' do
         @product.explain = ''
         @product.valid?
-        expect(@product.errors.full_messages).to include("Explain can't be blank")
+        expect(@product.errors.full_messages).to include("商品の説明を入力してください")
       end
 
       it 'カテゴリーの情報がないと登録できない' do
         @product.category_id = '1'
         @product.valid?
-        expect(@product.errors.full_messages).to include("Category can't be blank")
+        expect(@product.errors.full_messages).to include("カテゴリーを選択してください。")
       end
 
       it '商品の状態についての情報がないと登録できない' do
         @product.status_id = '1'
         @product.valid?
-        expect(@product.errors.full_messages).to include("Status can't be blank")
+        expect(@product.errors.full_messages).to include("商品の状態を選択してください。")
       end
 
       it '配送料の負担についての情報がないと登録できない' do
         @product.shipping_cost_id = '1'
         @product.valid?
-        expect(@product.errors.full_messages).to include("Shipping cost can't be blank")
+        expect(@product.errors.full_messages).to include("配送料の負担を選択してください。")
       end
 
       it '発送元の地域についての情報がないと登録できないと' do
         @product.region_id = '1'
         @product.valid?
-        expect(@product.errors.full_messages).to include("Region can't be blank")
+        expect(@product.errors.full_messages).to include("発送元の地域を選択してください。")
       end
 
       it '発送までの日数についての情報がないと登録できないと' do
         @product.shipping_date_id = '1'
         @product.valid?
-        expect(@product.errors.full_messages).to include("Shipping date can't be blank")
+        expect(@product.errors.full_messages).to include("発送までの日数を選択してください。")
       end
 
       it '販売価格についての情報がないと登録できない' do
         @product.price = ''
         @product.valid?
-        expect(@product.errors.full_messages).to include("Price can't be blank")
+        expect(@product.errors.full_messages).to include("販売価格を入力してください")
       end
 
       it '販売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
         @product.price = '100'
         @product.valid?
-        expect(@product.errors.full_messages).to include('Price is invalid')
+        expect(@product.errors.full_messages).to include('販売価格is invalid')
       end
 
       it '販売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
         @product.price = '10,000,000'
         @product.valid?
-        expect(@product.errors.full_messages).to include('Price is invalid')
+        expect(@product.errors.full_messages).to include('販売価格is invalid')
       end
 
       it '販売価格は半角数字のみ保存可能であること' do
         @product.price = '１００００'
         @product.valid?
-        expect(@product.errors.full_messages).to include('Price is invalid')
+        expect(@product.errors.full_messages).to include('販売価格is invalid')
       end
     end
   end
--- spec/models/purchase_buyer_spec.rb
+++ spec/models/purchase_buyer_spec.rb
@@ -22,62 +22,62 @@ RSpec.describe PurchaseBuyer, type: :model do
       it 'post_codeが空だと購入できない' do
         @purchase_buyer.post_code = ''
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include('Post code is invalid')
+        expect(@purchase_buyer.errors.full_messages).to include('郵便番号を入力してください')
       end
       it 'post_codeは「3桁ハイフン4桁」の半角文字列のみ保存可能なこと' do
         @purchase_buyer.post_code = '1234567'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include('Post code is invalid')
+        expect(@purchase_buyer.errors.full_messages).to include("郵便番号は不正な値です")
       end
       it 'cityが空だと購入できない' do
         @purchase_buyer.city = ''
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("City can't be blank")
+        expect(@purchase_buyer.errors.full_messages).to include("市区町村を入力してください")
       end
-      it 'blockが空だと購入できない' do
+        it 'blockが空だと購入できない' do
         @purchase_buyer.block = ''
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("Block can't be blank")
+        expect(@purchase_buyer.errors.full_messages).to include("番地を入力してください")
       end
       it 'phoneが空だと購入できない' do
         @purchase_buyer.phone = ''
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include('Phone is invalid')
+        expect(@purchase_buyer.errors.full_messages).to include("電話番号を入力してください")
       end
       it 'phoneは9桁以下だと購入できない' do
         @purchase_buyer.phone = '123456789'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include('Phone is invalid')
+        expect(@purchase_buyer.errors.full_messages).to include("電話番号は不正な値です")
       end
       it 'phoneは12桁以上だと購入できない' do
         @purchase_buyer.phone = '123456789123'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include('Phone is invalid')
+        expect(@purchase_buyer.errors.full_messages).to include("電話番号は不正な値です")
       end
       it 'numberは半角数字以外が含まれている場合だと購入できない' do
         @purchase_buyer.phone = '090-1234'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include('Phone is invalid')
+        expect(@purchase_buyer.errors.full_messages).to include("電話番号は不正な値です")
       end
       it 'region_idに「1」が選択されている場合は出品できない' do
         @purchase_buyer.region_id = '1'
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("Region can't be blank")
+        expect(@purchase_buyer.errors.full_messages).to include("都道府県を選択してください")
       end
       it 'userが紐付いていなければ購入できない' do
         @purchase_buyer.user_id = nil
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("User can't be blank")
+        expect(@purchase_buyer.errors.full_messages).to include("Userを入力してください")
       end
       it 'productが紐付いていなければ購入できない' do
         @purchase_buyer.product_id = nil
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("Product can't be blank")
+        expect(@purchase_buyer.errors.full_messages).to include("Productを入力してください")
       end
       it 'tokenが空では登録できないこと' do
         @purchase_buyer.token = nil
         @purchase_buyer.valid?
-        expect(@purchase_buyer.errors.full_messages).to include("Token can't be blank")
+        expect(@purchase_buyer.errors.full_messages).to include("クレジットカード情報を入力してください")
       end
     end
   end
